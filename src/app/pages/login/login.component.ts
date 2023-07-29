ed52919959c294cfcfb195d75879ab026fb2a4fd
Daniel Corredor <dann95.moz@gmail.com>
2023-07-28 21:19:51 -0500

    conexion to collection

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import {  FormControl, Validators } from '@angular/forms'
 import { Router } from '@angular/router';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable, tap } from 'rxjs';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
@@ -9,12 +11,16 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   constructor(private router: Router) {
- 
-   }
+    const itemCollection = collection(this.firestore, 'items');
+    this.item$ = collectionData(itemCollection).pipe(tap(console.log));
+  }
+  
   ngOnInit() {
   }
   email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [Validators.required])
+  item$: Observable<any[]>;
+  firestore: Firestore = inject(Firestore);
   loginWithFacebook() {
   
   }
