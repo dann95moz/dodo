b4712dca97916ddcb8362744e1018ae44d27509a
Daniel Corredor <dann95.moz@gmail.com>
2023-07-28 19:24:36 -0500

    angular fire installation ok

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'dodo-393116',
+    appId: '1:31960961697:web:5d086a991ee2c844bcd6e0',
+    databaseURL: 'https://dodo-393116-default-rtdb.firebaseio.com',
+    storageBucket: 'dodo-393116.appspot.com',
+    apiKey: 'AIzaSyDaBXG_eNEytzYHhcHAhqLMrlSeSLwidVI',
+    authDomain: 'dodo-393116.firebaseapp.com',
+    messagingSenderId: '31960961697',
+    measurementId: 'G-79MYJM5WNH',
+  },
+};
