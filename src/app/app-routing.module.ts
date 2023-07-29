3d847f08e9f2d96ea35869251e1534f0e8de172e
Daniel Corredor <dann95.moz@gmail.com>
2023-07-28 19:45:20 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursesComponent } from './pages/home/courses/courses.component';
+import { MentorsComponent } from './pages/home/mentors/mentors.component';
+import { PlansComponent } from './pages/home/plans/plans.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    {
+      path: 'login', component: LoginComponent
+    },
+    { path: 'home', component: HomeComponent , children: [
+      { path: 'courses', component: CoursesComponent },
+      { path: 'mentors', component: MentorsComponent },
+      {path: 'plans', component: PlansComponent },
+    ] },
+    
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
