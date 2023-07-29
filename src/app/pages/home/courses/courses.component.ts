11e8534f53bfbaf692c8455f1fab8dd900210252
Daniel Corredor <dann95.moz@gmail.com>
2023-07-28 22:02:47 -0500

    logout

--- src/app/pages/home/courses/courses.component.ts
+++ src/app/pages/home/courses/courses.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import {  signOut } from 'firebase/auth';
 import { Router } from '@angular/router';
+import { Auth } from '@angular/fire/auth';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.sass']
 })
 export class CoursesComponent {
-  constructor(private router: Router){}
+  constructor(private router: Router, private auth:Auth){}
   logout() {
-
+    signOut(this.auth).then(() => {
+    
+      this.router.navigate(['/login']);
+    })
 }
 }
